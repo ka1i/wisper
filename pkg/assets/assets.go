15e1b0dff96b0c88f92dd7987831e6640400ccd3
Mardan M <Mardan137@outlook.com>
2021-05-07 21:17:02 +0800

    fix assets loss

--- pkg/assets/assets.go
+++ pkg/assets/assets.go
@@ -2,5 +2,9 @@ package assets
 
 import "embed"
 
-//go:embed web/*
+//go:embed web
+//go:embed web/_next/static
+//go:embed web/_next/static/chunks/pages/*.js
+//go:embed web/_next/static/*/*.js
+//go:embed web/_next/static/*/*.css
 var Storage embed.FS
