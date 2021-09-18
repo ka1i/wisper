ab412dfe7b2698eb1aee08ede0095f9276610c55
Mardan <Mardan137@outlook.com>
2021-09-18 15:32:56 +0800

    Update buildvda.sh

--- buildvda.sh
+++ buildvda.sh
@@ -15,18 +15,5 @@ sudo mount vda.img /mnt/vda
 prefix=$(pwd)
 cd /mnt/vda
 sudo tar xf ${prefix}/alpine-minirootfs-3.14.2-x86_64.tar.gz
-sudo rm -rf etc/conf.d etc/logrotate.d etc/modprobe.d etc/modules-load.d etc/network etc/opt etc/sysctl.d lib/modules-load.d
-sudo rm etc/modules etc/udhcpd.conf etc/sysctl.conf etc/hostname etc/motd etc/issue etc/shadow
-sudo cp /etc/passwd etc/passwd
-sudo cp /etc/group etc/group
-sudo cp /etc/hosts etc/hosts
-sudo cp /etc/inittab etc/inittab
-sudo cp /etc/fstab etc/fstab
-sudo cp -Tr ../etc/init.d etc/init.d
-sudo mkdir etc/dropbear
-sudo cp -Tr /root root
-sudo cp /usr/share/udhcpc/default.script usr/share/udhcpc/default.script
-sudo rm -rf lib/sysctl.d
-sudo rm -rf media opt srv
 
 sudo umount /mnt/vda
