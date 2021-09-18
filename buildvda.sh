0ad648af1fb7fe54177f6e9954b25a19e8d96361
Mardan <Mardan137@outlook.com>
2021-09-18 15:26:34 +0800

    Update buildvda.sh

--- buildvda.sh
+++ buildvda.sh
@@ -7,25 +7,26 @@ if [ ! -f alpine-minirootfs-3.14.2-$ARCH.tar.gz ]; then
     wget https://dl-cdn.alpinelinux.org/alpine/v3.14/releases/x86_64/alpine-minirootfs-3.14.2-x86_64.tar.gz
 fi
 
-umount mnt
 dd if=/dev/zero of=vda.img bs=1M count=512
 mkfs.ext4 vda.img
-mkdir mnt
-sudo mount vda.img mnt
-cd mnt
-tar xf ../alpine-minirootfs-3.14.2-x86_64.tar.gz
+mkdir /mnt/vda
+sudo mount vda.img /mnt/vda
+
+prefix=$(pwd)
+cd /mnt/vda
+sudo tar xf ${prefix}/alpine-minirootfs-3.14.2-x86_64.tar.gz
 rm -rf etc/conf.d etc/logrotate.d etc/modprobe.d etc/modules-load.d etc/network etc/opt etc/sysctl.d lib/modules-load.d
 rm etc/modules etc/udhcpd.conf etc/sysctl.conf etc/hostname etc/motd etc/issue etc/shadow
-cp ../etc/passwd etc/passwd
-cp ../etc/group etc/group
-cp ../etc/hosts etc/hosts
-cp ../etc/inittab etc/inittab
-cp ../etc/fstab etc/fstab
+cp /etc/passwd etc/passwd
+cp /etc/group etc/group
+cp /etc/hosts etc/hosts
+cp /etc/inittab etc/inittab
+cp /etc/fstab etc/fstab
 cp -Tr ../etc/init.d etc/init.d
 mkdir etc/dropbear
-cp -Tr ../root root
-cp ../usr/share/udhcpc/default.script usr/share/udhcpc/default.script
+cp -Tr /root root
+cp /usr/share/udhcpc/default.script usr/share/udhcpc/default.script
 rm -rf lib/sysctl.d
 rm -rf media opt srv
-cd ..
-umount mnt
+
+umount /mnt/vda
